56b783e3640e1177c8aec3a0157897f7787ef506
Denis <niskadevla@gmail.com>
2020-09-16 16:24:01 +0300

    US004: Added link to user info

--- src/app/core/routes/app-routes.ts
+++ src/app/core/routes/app-routes.ts
@@ -4,6 +4,7 @@ export const routes: IAppRotes = {
     LOGIN: { routerPath: 'login' },
     REGISTRATION: { routerPath: 'registration', fullPath: '/registration' },
     HEROES: { routerPath: 'heroes' },
-    SELECTION_PAGE: { routerPath: 'selection-page' }
+    SELECTION_PAGE: { routerPath: 'selection-page' },
+    USER_INFO: { routerPath: 'user-info' },
 };
 
