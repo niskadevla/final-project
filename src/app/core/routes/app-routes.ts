e2dea4d04a3174a795f75b6d55be981f8f038230
Denis <niskadevla@gmail.com>
2020-09-21 15:24:16 +0300

    US007: Created Heroes list tab

--- src/app/core/routes/app-routes.ts
+++ src/app/core/routes/app-routes.ts
@@ -4,10 +4,11 @@ export const routes: IAppRotes = {
     LOGIN: { routerPath: 'login' },
     REGISTRATION: { routerPath: 'registration', fullPath: '/registration' },
     HEROES: { routerPath: 'heroes' },
-    SELECTION_PAGE: { routerPath: 'selection-page', fullPath: 'heroes/selection-page'},
+    SELECTION_PAGE: { routerPath: 'selection-page', fullPath: '/heroes/selection-page'},
     USER_INFO_PAGE: { routerPath: 'user-info-page', fullPath: 'heroes/user-info-page' },
     USER_HEROES_LIST: { routerPath: 'user-heroes-list', fullPath: 'heroes/user-info-page/user-heroes-list' },
     BATTLES_HISTORY: { routerPath: 'battles-history' },
-    POWERUPS: { routerPath: 'powerups' }
+    POWERUPS: { routerPath: 'powerups' },
+    HERO_INFO_PAGE: { routerPath: 'hero-info-page'}
 };
 
--- src/app/features/heroes/components/user-info-page/components/user-heroes-list/user-heroes-list.module.ts
+++ src/app/features/heroes/components/user-info-page/components/user-heroes-list/user-heroes-list.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 import { UserHeroesListComponent } from './user-heroes-list.component';
-
 
 @NgModule({
   declarations: [
       UserHeroesListComponent
   ],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
     exports: [
         UserHeroesListComponent
     ]
