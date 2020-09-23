5d2a87153cefdcfb22edd6b146920f5034f4e779
Denys Bielishev <48553334+niskadevla@users.noreply.github.com>
2020-09-23 10:02:07 +0300

    us008 created history tab (#12)
    US008: Created History tab

--- src/app/shared/models/user-info.ts
+++ src/app/shared/models/user-info.ts
@@ -1,16 +1,19 @@
 import { IHero } from './hero.model';
+import { IBattleHistory } from './battle-history.model';
 
 export interface IUserInfo {
     searchQuery: string;
     selectedHeroes: IHero[];
     selectedLetter: string;
+    battlesHistory?: IBattleHistory[];
 }
 
 export class UserInfo implements IUserInfo {
     constructor(
         public searchQuery: string,
         public selectedHeroes: IHero[],
-        public selectedLetter: string = 'A'
+        public selectedLetter: string = 'A',
+        public battlesHistory?: IBattleHistory[],
     ) {
     }
 }
